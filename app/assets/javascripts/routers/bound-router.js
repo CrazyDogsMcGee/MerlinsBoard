5ab605d1951f8e86caed7cb2b311441dad9b1169
Jonathan <l33.jonathan@gmail.com>
2015-03-26 17:57:43 -0400

    Decides on using Backbone.Router.execute for handling before route functions

--- app/assets/javascripts/routers/bound-router.js
+++ app/assets/javascripts/routers/bound-router.js
@@ -1,11 +1,34 @@
 MerlinsBoard.Routers.BoundRouter = Backbone.Router.extend({
   bindFilter: function (homeRoutes, detailRoutes) {
-
-
     this.on("route",function (event, args) {
       console.log("An event happened")
       console.log(event)
       console.log(args) //the first argument is always the courseid, or whatever comes first i nthe route
     })
-  }
+  },
+
+
 })
+
+// Backbone.Router.extend({
+// execute: function(callback, args) {
+//     var actionName = this.getActionName(callback);
+//     // super
+//     Backbone.Router.prototype.execute.apply(this, arguments);
+// },
+// getActionName: function(callback) {
+//     if (!this.routes) {
+//         return;
+//     }
+//
+//     var actionName;
+//     var matched;
+//     for (var routePattern in this.routes) {
+//         actionName = this.routes[routePattern];
+//         if (callback === this[actionName]) {
+//             matched = actionName;
+//             break;
+//         }
+//     }
+//     return matched;
+// }
--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -15,6 +15,32 @@ MerlinsBoard.Routers.Router = Backbone.Router.extend({
 
   },
 
+  execute: function(callback, args) {
+      var actionName = this.getActionName(callback);
+      console.log(actionName);
+
+      //concat fetched course with arguments and pass to function
+      // super
+      Backbone.Router.prototype.execute.apply(this, arguments);
+  },
+
+  getActionName: function(callback) {
+      if (!this.routes) {
+          return;
+      }
+
+      var actionName;
+      var matched;
+      for (var routePattern in this.routes) { //goes through all values (function names)
+          actionName = this.routes[routePattern];
+          if (callback === this[actionName]) {
+              matched = actionName;
+              break;
+          }
+      }
+      return matched;
+  },
+
 	routes: {
     //course resources
     "course/search" : "enrollcourses",
