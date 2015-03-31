aa4a2d7677d33afa0c035b4ca1d44ee9642dd190
CrazyDogsMcgee <l33.jonathan@gmail.com>
2015-03-31 14:27:49 +0000

    Execution router can successfully pass a reference of a fetched course to the event aggregator and route callback

--- app/assets/javascripts/routers/bound-router.js
+++ app/assets/javascripts/routers/bound-router.js
@@ -1,7 +1,7 @@
 MerlinsBoard.Routers.BoundRouter = Backbone.Router.extend({
-  routeRegex: function (routeName) {
-    var courseFlag = new Regexp("-course");
-    var homeFlag = new Regexp("-home");
+   routeRegex: function (routeName) {
+    var courseFlag = new RegExp("_course");
+    var homeFlag = new RegExp("_home");
     
     if (courseFlag.test(routeName)) {
       return "course" 
@@ -14,18 +14,20 @@ MerlinsBoard.Routers.BoundRouter = Backbone.Router.extend({
 
   execute: function (callback, args) {
     var actionName = this.getActionName(callback);
-    var noNullArgs = this.cleanArgs(args);
     
-    if (this.routeRegex(actionName) == "course") {
-      MerlinsBoard.Models._currentCourse = MerlinsBoard.Courses.getOrFetch(noNull[0]);
-      MerlinsBoard.Models._currentCourse.fetch();
-      
+    var noNull = _.filter(args, function (arg) {
+      return !(arg === null)
+    })
+    
+    if (this.routeRegex(actionName) == "course") { //should probably extract this somewhere else
+      this._currentCourse = MerlinsBoard.Courses.getOrFetch(noNull[0]);
       MerlinsBoard.Vent.trigger("courseRender",{courseModel: this._currentCourse});
+      this._currentCourse.fetch({success: function () {console.log(this._currentCourse)}.bind(this)});
     } else {
       MerlinsBoard.Vent.trigger("homeRender");
     }
     
-    if (callback) callback.apply(this, noNullArgs);
+    if (callback) callback.apply(this, noNull);
   },
   
   getActionName: function(callback) {
@@ -43,13 +45,5 @@ MerlinsBoard.Routers.BoundRouter = Backbone.Router.extend({
         }
     }
     return matched;
-  },
-  
-  cleanArgs: function (args) {
-    _.filter(args, function (arg) {
-      return !(arg === null)
-    })
-    
-    return args
   }
 })
