e070687b3b077ef9d8c72e7602f26876f1f29482
CrazyDogsMcgee <l33.jonathan@gmail.com>
2015-04-10 17:35:11 +0000

    Changes default profile picture for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
                   }
 
   #paperclip
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50#" }, :default_url => "https://s3.amazonaws.com/merlinsboardapp/tumblr_n7i34p1rfX1qzdeo7o1_1280.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50#" }, :default_url => "https://s3.amazonaws.com/merlinsboardapp/no_image.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   #Validators
